be246594f785edc4dd703c60901dfa43d0408922
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-13 10:29:06 +0600

    Input_output

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int h,w;
+
+    cout << "Enter the working hours: ";
+    cin >> h;
+    cout << "Enter the wage: ";
+    cin >> w;
+    cout << "The employees gross pay is: " << h*w;
+
+    return 0;
+}
