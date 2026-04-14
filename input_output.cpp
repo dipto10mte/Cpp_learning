b820e56af351d8c0746b569cc9ab10410fa59390
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-14 22:38:11 +0600

    Update

--- input_output.cpp
+++ input_output.cpp
@@ -1,3 +1,4 @@
+// this code calculates  an employee's gross pay from work hours and the employee's wage
 #include <iostream>
 using namespace std;
 
@@ -5,10 +6,13 @@ int main()
 {
     int h,w;
 
+    //Work hour input
     cout << "Enter the working hours: ";
     cin >> h;
+    //Wage input
     cout << "Enter the wage: ";
     cin >> w;
+    //Gross pay output
     cout << "The employees gross pay is: " << h*w;
 
     return 0;
