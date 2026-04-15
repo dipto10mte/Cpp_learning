1f0a88c5070d215aa1d0bf8ccd5f98879c190f59
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-15 23:36:19 +0600

    Update

--- Lowest_common_dominator.cpp
+++ Lowest_common_dominator.cpp
@@ -1,13 +1,19 @@
+//This program calculates lowest common dominator of two numbers
 #include <iostream>
 using namespace std;
+
  int main()
  {
     int a,b,d,m;
+
+    //Input two numbers
     cout << "Enter two numbers: ";
     cin >> a >> b;
+
+    //Calculate and output the dominator
     m=min(a,b);
     for(d=2;d<m;d++)if(((a%d)==0)&&((b%d)==0))break;
-    if(d==m){
+    if(d==m){ //In case if the found value is not dominator
         cout << "No common dominators";
         return 0;
     }
