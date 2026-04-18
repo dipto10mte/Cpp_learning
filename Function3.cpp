45249fef118267a00cabd19c616549e5d4e21c49
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-18 11:38:36 +0600

    Update

--- Function3.cpp
+++ Function3.cpp
@@ -32,24 +32,14 @@ void stack::push(char ch)
 }
 
 // Pop a character
-
-int main()
+char stack::pop()
 {
-    stack s1,s2; // create two stacks
-    int i;
-    // initialize the stacks
-    s1.init();
-    s2.init();
-
-    s1.push('a');
-    s2.push('x');
-    s1.push('b');
-    s2.push('y');
-    s1.push('c');
-    s2.push('z');
-
-    for(i=0;i<3;i++) cout << "Pop s1: " << s1.pop() << "\n";
-    for(i=0;i<3;i++) cout << "Pop s2: " << s2.pop() << "\n";
-
-    return 0;
+    if(tos==0){
+        cout << "stack is empty";
+        return 0; // return null on empty stack
+    }
+    tos--;
+    return stck[tos];
 }
+
+
