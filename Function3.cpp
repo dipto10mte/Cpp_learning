fb6a681a6ac6efd9688ba4f401033d8f12c34ed1
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-18 11:38:50 +0600

    Update

--- Function3.cpp
+++ Function3.cpp
@@ -42,4 +42,23 @@ char stack::pop()
     return stck[tos];
 }
 
+int main()
+{
+    stack s1,s2; // create two stacks
+    int i;
+    // initialize the stacks
+    s1.init();
+    s2.init();
 
+    s1.push('a');
+    s2.push('x');
+    s1.push('b');
+    s2.push('y');
+    s1.push('c');
+    s2.push('z');
+
+    for(i=0;i<3;i++) cout << "Pop s1: " << s1.pop() << "\n";
+    for(i=0;i<3;i++) cout << "Pop s2: " << s2.pop() << "\n";
+
+    return 0;
+}
