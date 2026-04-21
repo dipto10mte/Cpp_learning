914593d5df71d1f79e69d2219ec4bc3d48601433
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-21 19:45:20 +0600

    Update

--- Local_variable.cpp
+++ Local_variable.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
+    int i; // local variable declared at start of block
 
+    cout << "Enter number: ";
+    cin >> i;
+
+    // compute factorial
+    int j, fact=1; // variables are declared after action statements
+
+    for(j=i;j>=1;j--) fact=fact*j;
+
+    cout << "Factorial is: " << fact;
     return 0;
 }
