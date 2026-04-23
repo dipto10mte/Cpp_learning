380d3261b2721e33f5e6f3f5dc6a5500b9e25a46
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-23 13:03:07 +0600

    Update

--- overloading2.cpp
+++ overloading2.cpp
@@ -4,4 +4,11 @@ using namespace std;
 void date(char *date); // date as a string
 void date(int month, int day, int year); // date as a number
 
+int main()
+{
+    date("8/23/99");
+    date(8, 23, 99);
+
+    return 0;
+}
 
