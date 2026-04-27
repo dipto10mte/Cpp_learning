d0c0920ed9b2b6ffd90a3092bd6b01c3dfa9e5b9
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-04-27 14:51:07 +0600

    Update

--- Constructor_function2.cpp
+++ Constructor_function2.cpp
@@ -46,4 +46,19 @@ char stack::pop()
     return stck[tos];
 }
 
+int main()
+{
+    // create two stacks that are automatically initialized
+    stack s1,s2;
+    int i;
 
+    s1.push('a');
+    s2.push('x');
+    s1.push('b');
+    s2.push('y');
+    s1.push('c');
+    s2.push('z');
+
+    for(i=0;i<3;i++) cout << "Pop s1: " << s1.pop() << "\n";
+    for(i=0;i<3;i++) cout << "Pop s2: " << s2.pop() << "\n";
+}
