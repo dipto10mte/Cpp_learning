3bd345b4b2df879f6728046148bdd318a723bd03
dipto10mte <dipto2431010@stud.kuet.ac.bd>
2026-05-02 23:06:07 +0600

    Function

---
+++ Constructor_destructor_function1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+#define size 255
+
+class strtype{
+    char *p;
+    int len;
+public:
+    strtype(); // constructor
+    ~strtype(); // destructor
+    void set(char *ptr);
+    void show();
+};
+
+// Initialize a string object
+strtype::strtype()
+{
+    p=(char*) malloc(size);
+    if(!p){
+        cout << "Allocation error\n";
+        exit(1);
+    }
+    *p='\0';
+    len=0;
+}
+
